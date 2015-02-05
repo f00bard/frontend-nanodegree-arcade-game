3398c6cc4f469d8836311f77d09326c148e9deb8
f00bard <mack.hooper@gmail.com>
2015-02-04 21:40:48 -0500

    First step
    Game board displays on screen.

--- js/app.js
+++ js/app.js
@@ -24,12 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+}
+Player.prototype.update = function(dt) {
 
+}
+
+Player.prototype.render = function() {
+
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var numEnemies = 4;
+for (i=0; i < numEnemies; i++) {
+    allEnemies.push(new Enemy());
+}
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
